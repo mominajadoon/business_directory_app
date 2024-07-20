acd7df7254bcbf9f9e6ed56386a9847d42f9ceb0
mominajadoon <mominajadoon77@gmail.com>
2024-07-20 21:03:22 +0500

    updated

--- Models/User.js
+++ Models/User.js
@@ -1,20 +1,23 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  phone: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  otp: { type: Number },
-  isVerified: { type: Boolean, default: false },
-  role: { type: String, default: "user" },
-  favoriteBusinesses: {
-    type: [mongoose.Schema.Types.ObjectId],
-    default: [],
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    phone: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    otp: { type: Number },
+    isVerified: { type: Boolean, default: false },
+    role: { type: String, default: "user" },
+    favoriteBusinesses: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
+    favoriteEvents: {
+      type: [mongoose.Schema.Types.ObjectId],
+      default: [],
+    },
   },
-  favoriteEvents: {
-    type: [mongoose.Schema.Types.ObjectId],
-    default: [],
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
